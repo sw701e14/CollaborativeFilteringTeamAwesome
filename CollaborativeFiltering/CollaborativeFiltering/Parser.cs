09eb9845d15af691709ff1897f9984094c1b6e90
Bruno Thalmann <mr.thalmann@gmail.com>
2014-11-19 11:47:33 +0100

    done parsing

--- CollaborativeFiltering/CollaborativeFiltering/Parser.cs
+++ CollaborativeFiltering/CollaborativeFiltering/Parser.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CollaborativeFiltering
 {
-    class Parser
+    public static class Parser
     {
+
+        public static double[,] parseshit(string dirPath)
+        {
+            string[] lines = File.ReadAllLines(dirPath);
+            Dictionary<int, Tuple<int, int>> matrix = new Dictionary<int, Tuple<int, int>>();
+            foreach (var line in lines)
+            {
+                int movieId = -1;
+                if(line.Contains(':'))
+                    movieId = line.Split(':')[0].ToInt();
+                else
+                {
+                    string[] userInfo = line.Split(',');
+                    matrix.Add(movieId, Tuple.Create<int,int>(userInfo[0].ToInt(), userInfo[1].ToInt()));
+                }
+            }
+            throw new NotImplementedException  ();
+        }
+            public static int ToInt(this string str)
+            {
+                return Convert.ToInt32(str, 10);
+            }            
+
     }
 }
